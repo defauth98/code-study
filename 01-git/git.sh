2fa728efe8f850d5f8223774ebc2a21ab79eb38f
defauth98 <neto.daniribeiro@gmail.com>
2021-08-20 10:59:35 -0300

    Arquivos de estudo de git

---
+++ 01-git/git.sh
@@ -0,0 +1,23 @@
+# Verificar o estados dos arquivos 
+git status
+
+# Adicionar todos os arquivos ao git em stage
+git add .
+
+# Fazer commit das mudanças
+git commit -m "Mensagem do commit"
+
+# Enviar as mudanças para o repositório remoto
+git push
+
+# Visualizar o histórico de commits
+git log
+
+# Listar todas as branchs
+git branch
+
+# Ir para uma branch
+git checkout nomeDaBranch
+
+# Criar uma nova branch
+git checkout -b nomeDaBranch
