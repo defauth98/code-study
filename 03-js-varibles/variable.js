b19ba0f6e850731f728a779ebaab6a9766b79835
defauth98 <neto.daniribeiro@gmail.com>
2021-08-20 11:20:43 -0300

    Arquivos de estudo de variaveis em js

---
+++ 03-js-varibles/variable.js
@@ -0,0 +1,10 @@
+// jeito antigo de declarar variaveis
+// foge do escopo
+var variavel;
+
+// contantes no javascript
+const constante;
+
+// Tem que declarar antes de usar
+// Tem um escopo de bloco
+let variavel;
