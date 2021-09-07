14c75ebc3831999847645543a39d11072e0378bc
defauth98 <neto.daniribeiro@gmail.com>
2021-09-07 15:49:30 -0300

    Arquivos de estudo de arrays do javascript

---
+++ 09-arrays/arrayMethods.js
@@ -0,0 +1,15 @@
+// Métodos do array
+
+const frutas = ['Maçã', 'Banana'];
+
+frutas.push('Laranja') // Adiciona ao final 
+
+frutas.pop() // Remove do final 
+
+frutas.shift() // Remove do inicio 
+
+frutas.unshift('Goiaba') // Adiciona ao inicio
+
+frutas.indexOf('Maçã') // Retorna o index do elemento
+
+frutas.splice(0, 1) // Remove um item do array
---
+++ 09-arrays/arrays.js
@@ -0,0 +1,8 @@
+const frutas = ['Maçã', 'Banana'];
+
+console.log(frutas.length); // 2
+
+const primeiroItem = frutas[0]; // Maça
+
+// Como iterar sobre um array
+frutas.forEach(fruta => console.log(fruta))
