5f1b9ae8ba08d51e016b63ffce25571904084a28
defauth98 <neto.daniribeiro@gmail.com>
2021-09-08 02:15:02 -0300

    Arquivos de estudo de objetos do javascript

---
+++ 10-objects/objects.js
@@ -0,0 +1,10 @@
+const newObject = {
+  name: "Objeto",
+  criar: () => {
+    console.log("Função que cria um objeto")
+  }
+}
+
+const keys = Object.keys(newObject);
+
+const values = Object.values(newObject);
