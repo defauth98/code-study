de5a89892bf7213d065b8288cb2ad71236e753ce
defauth98 <neto.daniribeiro@gmail.com>
2021-09-09 07:56:01 -0300

    Arquivos de estudo de then e catch, try e catch no javascript

---
+++ 17-then-catch-try/then-catch.js
@@ -0,0 +1,13 @@
+new Promise((resolve, reject) => {
+  console.log('Initial');
+
+  resolve();
+})
+  .then(() => {
+    throw new Error('Something failed');
+
+    console.log('Do this');
+  })
+  .catch(() => {
+    console.log('Do that');
+  })
---
+++ 17-then-catch-try/try-catch.js
@@ -0,0 +1,8 @@
+try {
+  const result = syncDoSomething();
+  const newResult = syncDoSomethingElse(result);
+  const finalResult = syncDoThirdThing(newResult);
+  console.log(`Got the final result: ${finalResult}`);
+} catch (error) {
+  failureCallback(error);
+}
